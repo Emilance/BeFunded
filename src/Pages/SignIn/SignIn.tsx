09901feb831d7d13f940e9562e2a485daeb19493
Devyoma <lawrenceyoma@gmail.com>
2022-11-07 08:24:10 +0100

    feat: set up react router

---
+++ src/Pages/SignIn/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SignIn.tsx';
+
+const SignIn = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
---
+++ src/Pages/SignUp/Signup.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../component/header/Header';
+import './Signup.css';
+
+const Signup = () => {
+  return (
+    <div className='signUp'>
+      <Header />
+
+      <div className="signUp__container">
+        <h1>Create Account</h1>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Signup
